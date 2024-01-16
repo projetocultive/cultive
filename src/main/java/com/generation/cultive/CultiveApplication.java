2b918e47670d1a16f1112148c22634d44821c06b
johnnataa <johnnata_@outlook.com>
2024-01-16 11:35:23 -0300

    Classe Categoria

---
+++ src/main/java/com/generation/cultive/CultiveApplication.java
@@ -0,0 +1,13 @@
+package com.generation.cultive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/cultive/CultiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.cultive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
