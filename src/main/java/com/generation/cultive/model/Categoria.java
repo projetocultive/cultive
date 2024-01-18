2c4ae617dccb146f4dc23c254a472354b89b20e4
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-01-18 17:23:01 -0300

    CRUD Categoria.

---
+++ src/main/java/com/generation/cultive/model/Categoria.java
@@ -0,0 +1,51 @@
+package com.generation.cultive.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Size(min = 3, max = 50)
+	@NotBlank(message = "O Nome é obrigatório!")
+	private String nome;
+
+	@Size(min = 10, max = 1000, message = "A descrição deve ter no mínimo 10 caracteres e no máximo 1000")
+	@NotBlank(message = "A Descrição é obrigatória!")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
