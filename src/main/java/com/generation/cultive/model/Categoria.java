1a9363f256b7987bf1ffe38db9b70013f3b8196c
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2024-01-24 14:53:58 -0300

    ajustes relacionamento entre classes

--- src/main/java/com/generation/cultive/model/Categoria.java
+++ src/main/java/com/generation/cultive/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.cultive.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@Size(min = 10, max = 1000, message = "A descrição deve ter no mínimo 10 caracteres e no máximo 1000")
 	@NotBlank(message = "A Descrição é obrigatória!")
 	private String descricao;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Categoria> categoria;
 
 	public Long getId() {
 		return id;
